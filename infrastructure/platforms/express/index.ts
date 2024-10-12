d9e27693784575591890b4798b6f4e5b613bd056
aminnairi <18418459+aminnairi@users.noreply.github.com>
2024-10-12 20:11:38 +0200

    exported the books routes in their own router

--- infrastructure/platforms/express/index.ts
+++ infrastructure/platforms/express/index.ts
@@ -1,14 +1,12 @@
-import { ListBooks } from "@bookstore/application/usecases/ListBooks";
 import express from "express";
 import { open } from "sqlite";
 import { Database } from "sqlite3";
 import { SqliteBookRepository } from "./adapters/repositories/SqliteBookRepository";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { AddBook } from "@bookstore/application/usecases/AddBook";
 import { ResendBookNotificationService } from "./adapters/services/ResendBookNotificationService";
 import { Resend } from "resend";
-import { InvalidIsbn } from "@bookstore/domain/errors/InvalidIsbn";
+import { createBooksRouter } from "./routers/books";
 
 const main = async () => {
   const port = 8000;
@@ -28,52 +26,9 @@ const main = async () => {
   const bookRepository = new SqliteBookRepository(database);
   const bookNotificationService = new ResendBookNotificationService(resend);
 
-  server.use(cors({
-    origin: "*"
-  }));
-
+  server.use(cors({ origin: "*" }));
   server.use(bodyParser.json());
-
-  server.get("/books", async (request, response) => {
-    try {
-      const listBooks = new ListBooks(bookRepository);
-      const books = await listBooks.execute();
-
-      if (books.length === 0) {
-        response.status(204).end();
-        return;
-      }
-
-      response.json({
-        books
-      });
-    } catch (error) {
-      response.status(500).json({
-        error: String(error)
-      });
-    }
-  });
-
-  server.post("/books", async (request, response) => {
-    try {
-      const { title, isbn, available_copies } = request.body;
-      const addBook = new AddBook(bookRepository, bookNotificationService);
-
-      await addBook.execute(title, isbn, available_copies);
-
-      response.status(201).end();
-    } catch (error) {
-      if (error instanceof InvalidIsbn) {
-        return response.status(400).json({
-          error: "ISBN is invalid, it should be 13 characters long."
-        });
-      }
-
-      response.status(500).json({
-        error: String(error)
-      });
-    }
-  });
+  server.use("/books", createBooksRouter(bookRepository, bookNotificationService));
 
   server.listen(port, host, () => {
     console.log(`Server listening on http://${host}:${port}`);
